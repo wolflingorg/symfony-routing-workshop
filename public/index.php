8a53ddac0d03126e09f2ff3be2f263a374fb58c7
Maksym Odanets <Maksym_Odanets@epam.com>
2018-11-21 19:15:11 +0200

    - YAML for routes

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+require_once '../vendor/autoload.php';
+
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+
+$fileLocator = new FileLocator(array(__DIR__));
+$loader = new YamlFileLoader($fileLocator);
+$collection = $loader->load('routes.yaml');
+
+$request = Request::createFromGlobals();
+
+$context = (new RequestContext())->fromRequest($request);
+
+$matcher = new UrlMatcher($collection, $context);
+
+try {
+    $defaults = $matcher->matchRequest($request);
+} catch (ResourceNotFoundException $e) {
+    echo $e->getMessage();
+}
+
+print_r($defaults);
