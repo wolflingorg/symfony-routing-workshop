97a02476a3d53b26a6c8c1eb25067faae24c51bd
Maksym Odanets <Maksym_Odanets@epam.com>
2018-11-22 13:25:37 +0200

    - small fix

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
+namespace src;
+
 require_once '../vendor/autoload.php';
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Routing\Loader\YamlFileLoader;
 use Symfony\Component\Routing\Matcher\Dumper\PhpMatcherDumper;
@@ -9,8 +10,6 @@ use Symfony\Component\Routing\Matcher\Dumper\PhpMatcherDumper;
 $fileLocator = new FileLocator(array(__DIR__));
 $loader = new YamlFileLoader($fileLocator);
 $collection = $loader->load('routes.yaml');
-
-$request = Request::createFromGlobals();
 
 $dumper = new PhpMatcherDumper($collection);
 echo $dumper->dump();
